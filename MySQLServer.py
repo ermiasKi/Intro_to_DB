5c63031d496f3e27d76dd14c36061f5a3f0c295b
ermias <ermiaskindalem97@gmail.com>
2025-08-17 12:02:00 +0300

    book store db

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+
+try:
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='******'
+    )
+
+    cursor = mydb.cursor()
+
+    cursor.execute('CREATE DATABASE alx_book_store;')
+
+    cursor.commit()
+    cursor.close()
+    mydb.close()
+
+    print("Database 'alx_book_store' created successfully!")
+except:
+    print("Database 'alx_book_store' has already createds!")
